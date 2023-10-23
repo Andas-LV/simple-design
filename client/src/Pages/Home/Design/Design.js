2f4af19f114ed3315180eab5dd79971a725ab6b5
Andas-LV <122715372+Andas-LV@users.noreply.github.com>
2023-10-23 16:48:52 +0600

    design

--- client/src/Pages/Home/Design/Design.js
+++ client/src/Pages/Home/Design/Design.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import {
   H1,
@@ -16,9 +16,47 @@ import {
   ItemButton,
 } from "../Design/Design.styled";
 import bag from "../../../Assets/img-mainpage/bag.svg";
-import { livingRooms, childrenRooms, corridors } from "../../../Components/rooms.data";
+import {
+  livingRooms,
+  childrenRooms,
+  corridors,
+} from "../../../Components/rooms.data";
 
 export default function Design() {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const handleMouseEnter = (i) => {
+    setHoveredIndex(i);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIndex(null);
+  };
+
+  function renderBlocks(title, data) {
+    return (
+      <Block key={title}>
+        <BlockName>
+          <H3>{title}</H3>
+          <Line></Line>
+        </BlockName>
+        <ItemWrapper>
+          {data.map((room) => (
+            <Item key={room.id}>
+              <img src={room.image} alt="img" />
+              <SubItem>
+                <H4>{room.name}</H4>
+                <Link to="/Catalog">
+                  <ItemButton>Выбрать</ItemButton>
+                </Link>
+              </SubItem>
+            </Item>
+          ))}
+        </ItemWrapper>
+      </Block>
+    );
+  }
+
   return (
     <Wrapper>
       <H1>Дизайн комнат</H1>
@@ -27,74 +65,9 @@ export default function Design() {
         интерьере, названиями, артикулами, цветом стен и так далее.
       </Text>
 
-      <Block>
-        <BlockName>
-          <H3>Гостиные</H3>
-          <Line></Line>
-        </BlockName>
-        <ItemWrapper>
-          {livingRooms.map((room) => (
-            <Item key={room.id}>
-              <img src={room.image} alt="img" />
-              <SubItem>
-                <H4>{room.name}</H4>
-                <Link to="/Catalog">
-                  <ItemButton>
-                    <img src={bag} alt="img" />
-                    Выбрать
-                  </ItemButton>
-                </Link>
-              </SubItem>
-            </Item>
-          ))}
-        </ItemWrapper>
-      </Block>
-
-      <Block>
-        <BlockName>
-          <H3>Детские</H3>
-          <Line></Line>
-        </BlockName>
-        <ItemWrapper>
-        {childrenRooms.map((room) => (
-            <Item key={room.id}>
-              <img src={room.image} alt="img" />
-              <SubItem>
-                <H4>{room.name}</H4>
-                <Link to="/Catalog">
-                  <ItemButton>
-                    <img src={bag} alt="img" />
-                    Выбрать
-                  </ItemButton>
-                </Link>
-              </SubItem>
-            </Item>
-          ))}
-        </ItemWrapper>
-      </Block>
-
-      <Block>
-        <BlockName>
-          <H3>Коридоры - прихожие</H3>
-          <Line></Line>
-        </BlockName>
-        <ItemWrapper>
-          {corridors.map((room) => (
-            <Item key={room.id}>
-              <img src={room.image} alt="img" />
-              <SubItem>
-                <H4>{room.name}</H4>
-                <Link to="/Catalog">
-                  <ItemButton>
-                    <img src={bag} alt="img" />
-                    Выбрать
-                  </ItemButton>
-                </Link>
-              </SubItem>
-            </Item>
-          ))}
-        </ItemWrapper>
-      </Block>
+      {renderBlocks("Гостиные", livingRooms)}
+      {renderBlocks("Детские", childrenRooms)}
+      {renderBlocks("Коридоры - прихожие", corridors)}
 
       <Link to="/catalog">
         <Button>К каталогу комнат</Button>
